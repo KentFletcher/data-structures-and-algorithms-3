6934a398aa2cdde9f919e0651bee0ad9d88114aa
KentFletcher <kentfletch09@gmail.com>
2021-02-06 11:57:59 -0800

    completed challenge 8

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -104,6 +104,10 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 
 const evenOdd = (arr) => {
   // Solution code here...
+  return arr.map(ele => {
+    return typeof ele !== 'number' ? 'N/A'
+      : ele % 2 === 0 ? 'even' : 'odd';
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -268,7 +272,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual(['odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd']);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
