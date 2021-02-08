12c6884eb3d46d44ef35788a28f9c57915ff332a
KentFletcher <kentfletch09@gmail.com>
2021-02-08 09:31:07 -0800

    completed challenge 7

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -123,6 +123,7 @@ const snorlaxData = {
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
   // Solution code here...
+  return arr.filter(stat => stat.baseStat > minBaseStat)
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -284,7 +285,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([{ stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 }]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
