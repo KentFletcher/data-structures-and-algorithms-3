ffd8b8db745b85ac04a29c26934c1f67263083d6
KentFletcher <kentfletch09@gmail.com>
2021-02-09 11:55:41 -0800

    completed challenge 3

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -53,6 +53,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 const addPurchases = (arr) => {
   // Solution code here...
+  return arr.reduce((acc, curr) => acc + curr.purchasePrice, 0)
 };
 
 /* ------------------------------------------------------------------------------------------------
